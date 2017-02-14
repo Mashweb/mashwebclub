3aca571ba77e08a51544215d7932a7215221ddaf
Tom Elam <tomelam@gmail.com>
2017-02-14 13:13:42 +0530

    Edited config/routes.rb to permanently (301) redirect subdomain 'www'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  constraints(host: /^www\./i) do
+    get '(*any)' => redirect { |params, request|
+      URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '') }.to_s
+    }
+  end
   namespace :admin do
     resources :users
     root to: "users#index"
