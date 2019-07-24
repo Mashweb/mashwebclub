5f18d8f47e2cb2307590662b05f6480526ffb4e5
Tom Elam <tomelam@gmail.com>
2019-07-25 01:33:41 +0530

    Deleted an extraneous line.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,3 @@ config.action_mailer.smtp_settings = {
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
--  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
